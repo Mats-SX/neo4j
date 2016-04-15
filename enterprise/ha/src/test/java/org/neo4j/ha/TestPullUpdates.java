bade4ab438baf604d5e7a03b8247289b3c9cdb41
Mats Rydberg <mats@neotechnology.com>
2016-04-15 12:03:19 +0200

    Configure `neo4j_home` to outside store dir
    In embedded HA setups (common for our build/test environments), there
    are issues when starting a cluster if the `logs` directory is placed
    inside of the store directory. This commit configures the `logs`
    directory to be within a sibling `misc` directory to the store dir.

--- enterprise/ha/src/test/java/org/neo4j/ha/TestPullUpdates.java
+++ enterprise/ha/src/test/java/org/neo4j/ha/TestPullUpdates.java
@@ -38,6 +38,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.NotFoundException;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.graphdb.factory.TestHighlyAvailableGraphDatabaseFactory;
 import org.neo4j.helpers.collection.MapUtil;
 import org.neo4j.kernel.ha.HaSettings;
